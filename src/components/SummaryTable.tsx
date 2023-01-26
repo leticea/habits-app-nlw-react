322092658431de5cdf2cc90e364bd507fe8be80e
leticea <nascimento.leticeah@gmail.com>
2023-01-26 18:20:00 -0300

    editando os quadradinhos dos dias dos habitos

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -5,7 +5,8 @@ const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 
 const summaryDates = generateDatesFromYearBeginning();
 
-console.log(summaryDates);
+const minimumSummaryDatesSize = 18 * 7;
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
 export function SummaryTable() {
   return (
@@ -24,9 +25,19 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map(date => {
+        {summaryDates.map((date) => {
           return <HabitDay key={date.toString()} />;
         })}
+
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+            return (
+              <div
+                key={i}
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              />
+            );
+          })}
       </div>
     </div>
   );
