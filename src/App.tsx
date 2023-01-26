162e20793d206944f08271e6c7e7dd3d7af0e9a0
leticea <nascimento.leticeah@gmail.com>
2023-01-26 17:21:32 -0300

    inserindo imagem do habits e botão novo hábito

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./styles/global.css";
+import { Plus } from "phosphor-react";
 
 //import { Habit } from "./components/Habit";
 
@@ -11,7 +12,11 @@ export function App() {
         <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
           <img src={logoImage} alt="habits" />
 
-          <button type="button" className="border border-violet-500 font-semibold rounded-lg px-6 py-4">
+          <button
+            type="button"
+            className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
+          >
+            <Plus size={20} className="text-violet-500" />
             Novo hábito
           </button>
         </div>
