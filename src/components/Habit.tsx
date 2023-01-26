403baafde954e0fba0ffcc13a0369bef6692186a
leticea <nascimento.leticeah@gmail.com>
2023-01-26 11:38:18 -0300

    first commit

--- src/components/Habit.tsx
+++ src/components/Habit.tsx
@@ -1,9 +1,11 @@
 interface HabitProps {
-  completed: number
+  completed: number;
 }
 
 export function Habit(props: HabitProps) {
   return (
-    <div className="">{props.completed}</div>
-  )
+    <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 flex items-center justify-center">
+      {props.completed}
+    </div>
+  );
 }
