eb1f2f91d3cb54f9a8c1e3a28b6f284a9969abf5
leticea <nascimento.leticeah@gmail.com>
2023-01-27 12:02:56 -0300

    criando a base do formulário

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,9 +1,21 @@
+import * as Popover from "@radix-ui/react-popover";
+
 /*interface HabitDayProps {
   completed: number;
 }*/
 
 export function HabitDay() {
   return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+    <Popover.Root>
+      <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+          testeeeeeeee
+
+          <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   );
 }
