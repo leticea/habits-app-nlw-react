b9a83e3f3be6225132f49c6108c5e2201a1d5ced
leticea <nascimento.leticeah@gmail.com>
2023-01-27 10:46:31 -0300

    criando o método dialog trigger

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Plus } from "phosphor-react";
+import * as Dialog from "@radix-ui/react-dialog";
 
 import logoImage from "../assets/logo.svg";
 
@@ -7,13 +8,15 @@ export function Header() {
     <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
       <img src={logoImage} alt="habits" />
 
-      <button
-        type="button"
-        className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
-      >
-        <Plus size={20} className="text-violet-500" />
-        Novo hábito
-      </button>
+      <Dialog.Root>
+        <Dialog.Trigger
+          type="button"
+          className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
+        >
+          <Plus size={20} className="text-violet-500" />
+          Novo hábito
+        </Dialog.Trigger>
+      </Dialog.Root>
     </div>
   );
 }
