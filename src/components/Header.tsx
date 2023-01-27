b09021458ff211901a7d8e652a19cadb53b8197c
leticea <nascimento.leticeah@gmail.com>
2023-01-27 11:43:35 -0300

    criando a base do formulário

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
-import { Plus } from "phosphor-react";
+import { Plus, X } from "phosphor-react";
 import * as Dialog from "@radix-ui/react-dialog";
 
 import logoImage from "../assets/logo.svg";
+import { NewHabitForm } from "./NewHabitForm";
 
 export function Header() {
   return (
@@ -21,7 +22,15 @@ export function Header() {
           <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
 
           <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            Conteúdo do modal
+            <Dialog.Close className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-200">
+              <X size={24} aria-label="Fechar" />
+            </Dialog.Close>
+
+            <Dialog.Title className="text-3xl leading-tight font-extrabold">
+              Criar hábito
+            </Dialog.Title>
+
+            <NewHabitForm />
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
