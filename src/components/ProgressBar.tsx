e5d7df4ca7ef25a55079562ab654ebbe92883a81
leticea <nascimento.leticeah@gmail.com>
2023-01-27 12:38:35 -0300

    criando as cores dos quadradinhos de acordo com a porcentagem

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -9,7 +9,8 @@ export function ProgressBar(props: ProgressBarProps) {
         role="progressbar"
         aria-label="Progresso de hábitos completados nesse dia"
         aria-valuenow={props.progress}
-        className="h-3 rounded-xl bg-violet-600 w-3/4"
+        className="h-3 rounded-xl bg-violet-600"
+        style={{ width: `${props.progress}%` }}
       />
     </div>
   );
