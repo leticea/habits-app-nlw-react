a5d95b1cff060a4d114c2f242f45861c2a4bf0d9
leticea <nascimento.leticeah@gmail.com>
2023-01-27 18:32:12 -0300

    criando o checkbox

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,4 +1,5 @@
 import * as Popover from "@radix-ui/react-popover";
+import * as Checkbox from "@radix-ui/react-checkbox";
 import clsx from "clsx";
 import { ProgressBar } from "./ProgressBar";
 
@@ -13,17 +14,18 @@ export function HabitDay({ completed, amount }: HabitDayProps) {
   return (
     <Popover.Root>
       <Popover.Trigger
-        className={clsx(
-          "w-10 h-10 border-2 rounded-lg",
-          {
-            "bg-violet-900 border-violet-800": completedPercentage === 0,
-            "bg-violet-900 border-violet-700": completedPercentage > 0 && completedPercentage < 20,
-            "bg-violet-800 border-violet-600": completedPercentage >= 20 && completedPercentage < 40,
-            "bg-violet-700 border-violet-500": completedPercentage >= 40 && completedPercentage < 60,
-            "bg-violet-600 border-violet-500": completedPercentage >= 60 && completedPercentage < 80,
-            "bg-violet-500 border-violet-400": completedPercentage >= 80,
-          }
-        )}
+        className={clsx("w-10 h-10 border-2 rounded-lg", {
+          "bg-violet-900 border-violet-800": completedPercentage === 0,
+          "bg-violet-900 border-violet-700":
+            completedPercentage > 0 && completedPercentage < 20,
+          "bg-violet-800 border-violet-600":
+            completedPercentage >= 20 && completedPercentage < 40,
+          "bg-violet-700 border-violet-500":
+            completedPercentage >= 40 && completedPercentage < 60,
+          "bg-violet-600 border-violet-500":
+            completedPercentage >= 60 && completedPercentage < 80,
+          "bg-violet-500 border-violet-400": completedPercentage >= 80,
+        })}
       />
 
       <Popover.Portal>
@@ -34,6 +36,18 @@ export function HabitDay({ completed, amount }: HabitDayProps) {
           </span>
 
           <ProgressBar progress={completedPercentage} />
+
+          <div className="mt-6 flex flex-col gap-3">
+            <Checkbox.Root
+              className="flex items-center gap-3"
+            >
+              <Checkbox.Indicator />
+
+              <span className="font-semibold text-xl text-white leading-tight">
+                Beber 2L de água
+              </span>
+            </Checkbox.Root>
+          </div>
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
