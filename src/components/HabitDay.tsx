21e85b8682cb459d3b9b12613b888b5f93960a77
leticea <nascimento.leticeah@gmail.com>
2023-01-27 18:50:55 -0300

    criando o checkbox

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -2,6 +2,7 @@ import * as Popover from "@radix-ui/react-popover";
 import * as Checkbox from "@radix-ui/react-checkbox";
 import clsx from "clsx";
 import { ProgressBar } from "./ProgressBar";
+import { Check } from "phosphor-react";
 
 interface HabitDayProps {
   completed: number;
@@ -39,11 +40,28 @@ export function HabitDay({ completed, amount }: HabitDayProps) {
 
           <div className="mt-6 flex flex-col gap-3">
             <Checkbox.Root
-              className="flex items-center gap-3"
+              className="flex items-center gap-3 group"
             >
-              <Checkbox.Indicator />
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500  group-data-[state=checked]:border-green-500">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
 
-              <span className="font-semibold text-xl text-white leading-tight">
+              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+                Beber 2L de água
+              </span>
+            </Checkbox.Root>
+            <Checkbox.Root
+              className="flex items-center gap-3 group"
+            >
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500  group-data-[state=checked]:border-green-500">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+
+              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
                 Beber 2L de água
               </span>
             </Checkbox.Root>
