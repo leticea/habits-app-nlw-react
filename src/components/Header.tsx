070dc00d0260a9823d67073b03d80139aeca4ee6
leticea <nascimento.leticeah@gmail.com>
2023-01-27 11:27:52 -0300

    editando o conteúdo modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,7 @@ export function Header() {
         <Dialog.Portal>
           <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
 
-          <Dialog.Content>
+          <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
             Conteúdo do modal
           </Dialog.Content>
         </Dialog.Portal>
