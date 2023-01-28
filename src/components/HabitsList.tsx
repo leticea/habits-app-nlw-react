8aa9b7157442b82872158686fc08903ab4c3fa03
leticea <nascimento.leticeah@gmail.com>
2023-01-28 17:14:40 -0300

    criando função handleToggleHabit

--- src/components/HabitsList.tsx
+++ src/components/HabitsList.tsx
@@ -6,6 +6,7 @@ import { api } from "../lib/axios";
 
 interface HabitsListProps {
   date: Date;
+  onCompletedChanged: (completed: number) => void
 }
 
 interface HabitsInfo {
@@ -17,7 +18,7 @@ interface HabitsInfo {
   completedHabits: string[];
 }
 
-export function HabitsList({ date }: HabitsListProps) {
+export function HabitsList({ date, onCompletedChanged }: HabitsListProps) {
   const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>();
 
   useEffect(() => {
@@ -33,20 +34,28 @@ export function HabitsList({ date }: HabitsListProps) {
   }, []);
 
   async function handleToggleHabit(habitId: string) {
-    await api.patch(`/habits/${habitId}/toggle`)
+    await api.patch(`/habits/${habitId}/toggle`);
 
-    const isHabitAlreadyCompleted = habitsInfo?.completedHabits.includes(habitId)
+    const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId);
 
-    let completedHabits: string[] = []
+    let completedHabits: string[] = [];
+
     if (isHabitAlreadyCompleted) {
+      completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId);
 
     } else {
-
+      completedHabits = [...habitsInfo!.completedHabits, habitId];
     }
 
+    setHabitsInfo({
+      possibleHabits: habitsInfo!.possibleHabits,
+      completedHabits,
+    })
+
+    onCompletedChanged(completedHabits.length)
   }
 
-  const isDateInPast = dayjs(date).endOf("day").isBefore(new Date())
+  const isDateInPast = dayjs(date).endOf("day").isBefore(new Date());
 
   return (
     <div className="mt-6 flex flex-col gap-3">
