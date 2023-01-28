44ab924c97ac782cd3feec68b179f8b772690018
leticea <nascimento.leticeah@gmail.com>
2023-01-28 18:24:31 -0300

    editando o efeito nos layouts

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,9 +1,9 @@
 import * as Popover from "@radix-ui/react-popover";
 import clsx from "clsx";
-import { ProgressBar } from "./ProgressBar";
 import dayjs from "dayjs";
-import { HabitsList } from "./HabitsList";
 import { useState } from "react";
+import { ProgressBar } from "./ProgressBar";
+import { HabitsList } from "./HabitsList";
 
 interface HabitDayProps {
   date: Date;
@@ -11,7 +11,11 @@ interface HabitDayProps {
   amount?: number;
 }
 
-export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayProps) {
+export function HabitDay({
+  defaultCompleted = 0,
+  amount = 0,
+  date,
+}: HabitDayProps) {
   const [completed, setCompleted] = useState(defaultCompleted);
 
   const completedPercentage =
@@ -21,20 +25,24 @@ export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayPro
   const dayOfWeek = dayjs(date).format("dddd");
 
   function handleCompletedChanged(completed: number) {
-    setCompleted(completed)
+    setCompleted(completed);
   }
 
   return (
     <Popover.Root>
       <Popover.Trigger
         className={clsx(
-          "w-10 h-10 border=2 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-background",
+          "w-10 h-10 border-2 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background",
           {
-            "bg-zinc-900 border-violet-800": completedPercentage === 0,
-            "bg-violet-900 border-violet-700": completedPercentage > 0 && completedPercentage < 20,
-            "bg-violet-800 border-violet-600": completedPercentage >= 20 && completedPercentage < 40,
-            "bg-violet-700 border-violet-500": completedPercentage >= 40 && completedPercentage < 60,
-            "bg-violet-600 border-violet-500": completedPercentage >= 60 && completedPercentage < 80,
+            "bg-zinc-900 border-zinc-800": completedPercentage === 0,
+            "bg-violet-900 border-violet-700":
+              completedPercentage > 0 && completedPercentage < 20,
+            "bg-violet-800 border-violet-600":
+              completedPercentage >= 20 && completedPercentage < 40,
+            "bg-violet-700 border-violet-500":
+              completedPercentage >= 40 && completedPercentage < 60,
+            "bg-violet-600 border-violet-500":
+              completedPercentage >= 60 && completedPercentage < 80,
             "bg-violet-500 border-violet-400": completedPercentage >= 80,
           }
         )}
--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -1,6 +1,6 @@
 import { Check } from "phosphor-react";
 import * as Checkbox from "@radix-ui/react-checkbox";
-import { FormEvent, useState } from "react";
+import { FormEvent, useState, ChangeEvent } from "react";
 import { api } from "../lib/axios";
 
 const availableWeekDays = [
@@ -32,8 +32,11 @@ export function NewHabitForm() {
     setTitle("")
     setWeekDays([])
 
-    alert("Hábito criado!")
+    alert("Hábito criado com sucesso!");
+  }
 
+  function handleTitleChange(event: ChangeEvent<HTMLInputElement>) {
+    setTitle(event.target.value);
   }
 
   function handleToggleWeekDay(weekDay: number) {
@@ -59,10 +62,10 @@ export function NewHabitForm() {
         type="text"
         id="title"
         placeholder="Exercícios, dormir bem, etc..."
-        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
         autoFocus
         value={title}
-        onChange={event => setTitle(event.target.value)}
+        onChange={handleTitleChange}
       />
 
       <label htmlFor="" className="font-semibold leading-tight mt-4">
@@ -74,11 +77,11 @@ export function NewHabitForm() {
           return (
             <Checkbox.Root
               key={weekDay}
-              className="flex items-center gap-3 group"
+              className="flex items-center gap-3 group focus:outline-none"
               checked={weekDays.includes(index)}
               onCheckedChange={() => handleToggleWeekDay(index)}
             >
-              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500  group-data-[state=checked]:border-green-500">
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500  group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background">
                 <Checkbox.Indicator>
                   <Check size={20} className="text-white" />
                 </Checkbox.Indicator>
@@ -92,7 +95,7 @@ export function NewHabitForm() {
 
       <button
         type="submit"
-        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500"
+        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
       >
         <Check size={20} weight="bold" />
         Confirmar
