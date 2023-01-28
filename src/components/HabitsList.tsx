9f1e50af3767733d0d388bf3a0910b1ca88dbc68
leticea <nascimento.leticeah@gmail.com>
2023-01-28 16:27:44 -0300

    criando função handleToggleHabit

--- src/components/HabitsList.tsx
+++ src/components/HabitsList.tsx
@@ -1,4 +1,5 @@
 import * as Checkbox from "@radix-ui/react-checkbox";
+import dayjs from "dayjs";
 import { Check } from "phosphor-react";
 import { useEffect, useState } from "react";
 import { api } from "../lib/axios";
@@ -31,13 +32,31 @@ export function HabitsList({ date }: HabitsListProps) {
       });
   }, []);
 
+  async function handleToggleHabit(habitId: string) {
+    await api.patch(`/habits/${habitId}/toggle`)
+
+    const isHabitAlreadyCompleted = habitsInfo?.completedHabits.includes(habitId)
+
+    let completedHabits: string[] = []
+    if (isHabitAlreadyCompleted) {
+
+    } else {
+
+    }
+
+  }
+
+  const isDateInPast = dayjs(date).endOf("day").isBefore(new Date())
+
   return (
     <div className="mt-6 flex flex-col gap-3">
       {habitsInfo?.possibleHabits.map((habit) => {
         return (
           <Checkbox.Root
             key={habit.id}
+            onCheckedChange={() => handleToggleHabit(habit.id)}
             checked={habitsInfo.completedHabits.includes(habit.id)}
+            disabled={isDateInPast}
             className="flex items-center gap-3 group"
           >
             <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500  group-data-[state=checked]:border-green-500">
